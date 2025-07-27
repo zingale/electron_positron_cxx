557eddff77e6ea91ada8dbfe6222f0de1d83bbeb
Michael Zingale <michael.zingale@stonybrook.edu>
2025-07-26 20:55:45 -0400

    this seems to work

--- test_fermi.cpp
+++ test_fermi.cpp
@@ -1,12 +1,66 @@
-#include "fermi_integrals.H"
-
 #include <iostream>
+#include <print>
+
+#include "fermi_integrals.H"
+#include "difference_utils.H"
 
 int main() {
 
-    FermiIntegral<long double> f(0.5, 500, 100);
-    f.evaluate(2);
+    using RealT = long double;
 
-    std::cout << f << std::endl;
+    RealT h = 1.e-5L;
+
+    std::println("dF/dη");
+
+    for (auto k : {-0.5L, 0.5L, 1.5L, 2.5L}) {
+        for (auto eta : {-70.0L, 0.0L, 50.0L, 500.0L, 10000.0L}) {
+            for (auto beta : {0.0L, 1.e-3L, 30.0L, 100.0L}) {
+
+                FermiIntegral<RealT> f(k, eta, beta);
+                f.evaluate(2);
+
+                // check dF/dη
+                auto [diff, err] =
+                    adaptive_diff<RealT>([=] (RealT _eta) -> RealT
+                    {
+                        FermiIntegral<RealT> _f(k, _eta, beta);
+                        _f.evaluate(0);
+                        return _f.F;
+                    }, eta, h);
+
+                RealT rel_err = std::abs(f.dF_deta - diff) / std::abs(f.dF_deta);
+
+                std::println("k = {:5.2f}, eta = {:9.3f}, beta = {:7.3f}, dF/dη= {:15.8g}, error = {:15.8g}",
+                             k, eta, beta, f.dF_deta, rel_err);
+            }
+        }
+    }
+
+    std::println();
+    std::println("dF/dβ");
+
+    for (auto k : {-0.5L, 0.5L, 1.5L, 2.5L}) {
+        for (auto eta : {-70.0L, 0.0L, 50.0L, 500.0L, 10000.0L}) {
+            for (auto beta : {0.0L, 1.e-3L, 30.0L, 100.0L}) {
+
+                FermiIntegral<RealT> f(k, eta, beta);
+                f.evaluate(2);
+
+                // check dF/dβ
+                auto [diff, err] =
+                    adaptive_diff<RealT>([=] (RealT _beta) -> RealT
+                    {
+                        FermiIntegral<RealT> _f(k, eta, _beta);
+                        _f.evaluate(0);
+                        return _f.F;
+                    }, beta, h);
+
+                RealT rel_err = std::abs(f.dF_dbeta - diff) / std::abs(f.dF_dbeta);
+
+                std::println("k = {:5.2f}, eta = {:9.3f}, beta = {:7.3f}, dF/dβ = {:15.8g}, error = {:15.8g}",
+                             k, eta, beta, f.dF_dbeta, rel_err);
+            }
+        }
+    }
 
 }
