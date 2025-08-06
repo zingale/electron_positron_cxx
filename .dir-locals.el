6f1a03e6ba2cb31706bd2939c65bb4967e2da5b5
Michael Zingale <michael.zingale@stonybrook.edu>
2025-08-06 13:28:59 -0400

    add eta_pos to the output (#6)
    this also adds a new CI test that compares the output

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
 ((c++-mode
   (flycheck-gcc-include-path . (".." "." ))
-  (flycheck-clang-include-path . (".." "."))))
+  (flycheck-clang-include-path . (".." "." "/usr/lib/gcc/x86_64-redhat-linux/15/include/"))))
