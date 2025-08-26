df86e820bc1139889aae8836596079925fcfe305
Michael Zingale <michael.zingale@stonybrook.edu>
2025-08-26 07:15:50 -0400

    add the data for the main plot

---
+++ plots/create_data.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# precision
+
+make clean
+make PRECISION=DOUBLE QUAD_PTS=200
+./generate_maxwell_data
+
+make clean
+make PRECISION=LONG_DOUBLE QUAD_PTS=200
+./generate_maxwell_data
+
+make clean
+make PRECISION=FLOAT128 QUAD_PTS=200
+./generate_maxwell_data
+
+make clean
+make PRECISION=BOOST256 QUAD_PTS=200
+./generate_maxwell_data
+
+# quadrature points
+
+make clean
+make PRECISION=FLOAT128 QUAD_PTS=20
+./generate_maxwell_data
+
+make clean
+make PRECISION=FLOAT128 QUAD_PTS=50
+./generate_maxwell_data
+
+make clean
+make PRECISION=FLOAT128 QUAD_PTS=100
+./generate_maxwell_data
+
+make clean
+make PRECISION=FLOAT128 QUAD_PTS=200
+./generate_maxwell_data
+
+make clean
+make PRECISION=FLOAT128 QUAD_PTS=400
+./generate_maxwell_data
