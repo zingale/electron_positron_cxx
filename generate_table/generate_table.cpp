eb18d90c8f70ed5c8b71d6c5636c54e11e78db01
Michael Zingale <michael.zingale@stonybrook.edu>
2025-08-27 18:17:10 -0400

    this mostly works

--- generate_table/generate_table.cpp
+++ generate_table/generate_table.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "real_type.H"
+#include "electron_positron.H"
 #include "helmholtz.H"
 #include "util.H"
 
@@ -56,6 +57,9 @@ auto main() -> int
     std::vector<Helmholtz<real_t>> helm_v(T_pts * rho_pts);
     std::vector<EOSState<real_t>> eos_v(T_pts * rho_pts);
 
+    // we need one third derivative to compute via differencing
+    std::vector<real_t> d3p_drho2dT_v(T_pts * rho_pts);
+
     // we will have OpenMP schedule things such that each thread is
     // working on similar temperatures, to better load balance
 
@@ -76,7 +80,23 @@ auto main() -> int
 
             helm_v[index] = helm;
             eos_v[index] = eos;
+
+            // we need compute a derivative manually
+            ElectronPositronEOS<real_t> ep_eos;
+
+            // we'll do ∂/∂ρ(∂²p/∂ρ∂T)
+            const real_t eps{0.01_rt};
+            auto drho{eps * rho};
+            auto [deriv, _err] = fd::adaptive_diff<real_t>([&] (real_t _rho) -> real_t
+                {
+                    auto es_eps = ep_eos.pe_state(_rho, T, Ye);
+                    return es_eps.d2p_drhodT;
+                }, rho, drho);
+
+            d3p_drho2dT_v[index] = deriv;
+
         }
+
     }
 
     std::ofstream of(std::format("helm_table_p{}_q{}.dat", precision, qnpts));
@@ -92,21 +112,21 @@ auto main() -> int
 
     // next is the pressure derivative table
 
-    for (auto &e : eos_v) {
+    for (auto [e, d3p_drho2dT] : std::views::zip(eos_v, d3p_drho2dT_v)) {
         util::println("{:32.24g}  {:32.24g}  {:32.24g}  {:32.24g}",
-                      e.dp_drho, e.d2p_drho2, e.d2p_drhodT, e.d3p_drho2dT);
+                      e.dp_drho, e.d2p_drho2, e.d2p_drhodT, d3p_drho2dT);
     }
 
     // next is degeneracy parameter
 
-    for (auto &e : eos_v) {
+    for (auto & e : eos_v) {
         util::println("{:32.24g}  {:32.24g}  {:32.24g}  {:32.24g}",
                       e.eta, e.deta_drho, e.deta_dT, e.d2eta_drhodT);
     }
 
     // finally number density
 
-    for (auto &e : eos_v) {
+    for (auto & e : eos_v) {
         util::println("{:32.24g}  {:32.24g}  {:32.24g}  {:32.24g}",
                       e.n, e.dn_drho, e.dn_dT, e.d2n_drhodT);
     }
