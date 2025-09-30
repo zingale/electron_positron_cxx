73e08a9c0b7310a6ab7322249c566113b1f0d64c
Michael Zingale <michael.zingale@stonybrook.edu>
2025-09-30 13:48:35 -0400

    this is faster now

--- tests/test_eos_pressure.cpp
+++ tests/test_eos_pressure.cpp
@@ -15,29 +15,29 @@ auto main() -> int
 {
 
     // test ∂p/∂ρ
-    test_rho_deriv(&EOSState<real_t>::dpe_drho, &EOSState<real_t>::p_e, "p⁻");
-    test_rho_deriv(&EOSState<real_t>::dpp_drho, &EOSState<real_t>::p_pos, "p⁺");
-    test_rho_deriv(&EOSState<real_t>::dp_drho, &EOSState<real_t>::p, "p");
+    test_rho_deriv<1>(&EOSState<real_t>::dpe_drho, &EOSState<real_t>::p_e, "p⁻");
+    test_rho_deriv<1>(&EOSState<real_t>::dpp_drho, &EOSState<real_t>::p_pos, "p⁺");
+    test_rho_deriv<1>(&EOSState<real_t>::dp_drho, &EOSState<real_t>::p, "p");
 
     // test ∂p/∂T
-    test_T_deriv(&EOSState<real_t>::dpe_dT, &EOSState<real_t>::p_e, "p⁻");
-    test_T_deriv(&EOSState<real_t>::dpp_dT, &EOSState<real_t>::p_pos, "p⁺");
-    test_T_deriv(&EOSState<real_t>::dp_dT, &EOSState<real_t>::p, "p");
+    test_T_deriv<1>(&EOSState<real_t>::dpe_dT, &EOSState<real_t>::p_e, "p⁻");
+    test_T_deriv<1>(&EOSState<real_t>::dpp_dT, &EOSState<real_t>::p_pos, "p⁺");
+    test_T_deriv<1>(&EOSState<real_t>::dp_dT, &EOSState<real_t>::p, "p");
 
     // test ∂²p/∂ρ²
-    test_rho2_deriv(&EOSState<real_t>::d2pe_drho2, &EOSState<real_t>::p_e, "p⁻");
-    test_rho2_deriv(&EOSState<real_t>::d2pp_drho2, &EOSState<real_t>::p_pos, "p⁺");
-    test_rho2_deriv(&EOSState<real_t>::d2p_drho2, &EOSState<real_t>::p, "p");
+    test_rho2_deriv<2>(&EOSState<real_t>::d2pe_drho2, &EOSState<real_t>::p_e, "p⁻");
+    test_rho2_deriv<2>(&EOSState<real_t>::d2pp_drho2, &EOSState<real_t>::p_pos, "p⁺");
+    test_rho2_deriv<2>(&EOSState<real_t>::d2p_drho2, &EOSState<real_t>::p, "p");
 
     // test ∂²p/∂ρ∂T
-    test_rhoT_deriv(&EOSState<real_t>::d2pe_drhodT, &EOSState<real_t>::dpe_dT, "p⁻");
-    test_rhoT_deriv(&EOSState<real_t>::d2pp_drhodT, &EOSState<real_t>::dpp_dT, "p⁺");
-    test_rhoT_deriv(&EOSState<real_t>::d2p_drhodT, &EOSState<real_t>::dp_dT, "p");
+    test_rhoT_deriv<2>(&EOSState<real_t>::d2pe_drhodT, &EOSState<real_t>::dpe_dT, "p⁻");
+    test_rhoT_deriv<2>(&EOSState<real_t>::d2pp_drhodT, &EOSState<real_t>::dpp_dT, "p⁺");
+    test_rhoT_deriv<2>(&EOSState<real_t>::d2p_drhodT, &EOSState<real_t>::dp_dT, "p");
 
     // test ∂²p/∂T²
-    test_T2_deriv(&EOSState<real_t>::d2pe_dT2, &EOSState<real_t>::p_e, "p⁻");
-    test_T2_deriv(&EOSState<real_t>::d2pp_dT2, &EOSState<real_t>::p_pos, "p⁺");
-    test_T2_deriv(&EOSState<real_t>::d2p_dT2, &EOSState<real_t>::p, "p");
+    test_T2_deriv<2>(&EOSState<real_t>::d2pe_dT2, &EOSState<real_t>::p_e, "p⁻");
+    test_T2_deriv<2>(&EOSState<real_t>::d2pp_dT2, &EOSState<real_t>::p_pos, "p⁺");
+    test_T2_deriv<2>(&EOSState<real_t>::d2p_dT2, &EOSState<real_t>::p, "p");
 
     // test ∂³p/∂ρ³
     test_rho3_deriv(&EOSState<real_t>::d3pe_drho3, &EOSState<real_t>::dpe_drho, "p⁻");
